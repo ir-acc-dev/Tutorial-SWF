977db3c4acf1be9e284c26771893c0fa926f1551
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-10-25 12:44:06 -0500

    rest of frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from "./components/Header.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Header/>
+        <h1>App Component</h1>
+
+
+    </>
+  )
+}
+
+export default App
